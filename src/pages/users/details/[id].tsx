50e11b7529201b061f348304a8e0b241673cf4ee
numair1997 <152866939+numair1997@users.noreply.github.com>
2024-06-07 16:06:08 +0500

    removed unused code

--- src/pages/users/details/[id].tsx
+++ src/pages/users/details/[id].tsx
@@ -2,54 +2,19 @@ import { useRouter } from "next/router";
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import Adminlayout from '@layouts/admin-layout'
 import { superAdmin_and_AdminStaff } from "@utils/auth-utils";
 import Spinner from "@components/common/spinner/spinner";
 import CustomError from "@components/common/error/custom-error";
-import { Card, CardContent, CardHeader } from "@mui/material";
-import { AdminStaffPermissions, UserAnalyticsFilterBy } from "@utils/constants";
+import { AdminStaffPermissions } from "@utils/constants";
 import { useUserQuery } from "@data/users/user-query";
 import UserProfileCard from "@components/users/user-profile-card";
-import { useUserOrdersForAdminQuery } from "@data/orders/user-orders-for-admin-query";
-import { getUserDashboardFilterDate } from "@utils/helper-functions";
-import { useUserAnalyticsQuery } from "@data/analytics/user-analytics-query";
-
 
 const UserDetailPage = () => {
-    const [page, setPage] = useState<number>(1)
-    const [time, setTime] = useState<UserAnalyticsFilterBy>(UserAnalyticsFilterBy.MONTHLY)
-    const [dates, setDates] = useState<{ startDate: Date; endDate: Date }>({
-        startDate: getUserDashboardFilterDate(UserAnalyticsFilterBy.MONTHLY),
-        endDate: new Date()
-    })
-
     const router = useRouter();
 
     const { data: user, error: userError, isLoading: fetchingUser } = useUserQuery(router?.query?.id as string);
-    // const { data: orders, isLoading: fetchingOrders } = useUserOrdersForAdminQuery({
-    //     limit: Number(process.env.NEXT_PUBLIC_PAGINATED_QUERY_LIMIT),
-    //     page: page,
-    //     userId: router?.query?.id as string,
-    //     startDate: dates?.endDate ? dates?.startDate : null,
-    //     endDate: dates?.endDate ? dates?.endDate : null,
-    // });
-    // const { data: userAnalaytics } = useUserAnalyticsQuery({
-    //     userId: router?.query?.id as string,
-    //     filterBy: time as UserAnalyticsFilterBy
-    // })
-
-
-    const onPageChange = (event: React.ChangeEvent<unknown>, value: number) => {
-        setPage(value);
-    }
-
-    // functions 
-    const onStatusChange = (value: UserAnalyticsFilterBy) => {
-        setTime(value);
-        setDates({ startDate: getUserDashboardFilterDate(value), endDate: new Date() });
-    }
-
 
     if (fetchingUser) return <Spinner />
     if (userError) return <CustomError errorMsg={userError?.message} />
