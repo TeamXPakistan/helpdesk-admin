f258ce0817d33b7b7ef4827ca8b3b6ae8b839618
numair1997 <152866939+numair1997@users.noreply.github.com>
2024-06-07 17:49:28 +0500

    added firebase keys

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/7.9.1/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js')
 
 firebase.initializeApp({
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: "D"
+    apiKey: "AIzaSyA83XQaSGuZ5GuAPjZNR7FF7zDPb7iFVH8",
+    authDomain: "helpdesk-eeb0f.firebaseapp.com",
+    projectId: "helpdesk-eeb0f",
+    storageBucket: "helpdesk-eeb0f.appspot.com",
+    messagingSenderId: "68545883903",
+    appId: "1:68545883903:web:34fd512b6d8502ee11a6ca",
+    measurementId: "G-KHFMNR14K5"
 });
 
 const messaging = firebase.messaging();
--- src/utils/firebase/firebase.ts
+++ src/utils/firebase/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { check_notification_permission_toast } from "./notification";
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyA83XQaSGuZ5GuAPjZNR7FF7zDPb7iFVH8",
+    authDomain: "helpdesk-eeb0f.firebaseapp.com",
+    projectId: "helpdesk-eeb0f",
+    storageBucket: "helpdesk-eeb0f.appspot.com",
+    messagingSenderId: "68545883903",
+    appId: "1:68545883903:web:34fd512b6d8502ee11a6ca",
+    measurementId: "G-KHFMNR14K5"
 };
 export const app = initializeApp(firebaseConfig);
 
@@ -18,7 +18,7 @@ export const requestForToken = async () => {
     const messaging = getMessaging(app);
     try {
         check_notification_permission_toast()
-        const token = await getToken(messaging, { vapidKey: "BLWfKeEQQMrsqoIdW5UrMlhIZw05Livs_ZUvnwGup3WdVOWQj9copZZnpbayHIHwr0bYck2gwzeWzd7F-iCNQNE" })
+        const token = await getToken(messaging, { vapidKey: "BK06WKZj6iWeP6rdZtSpbePbNQprSTIApEVfIQoORyFKpozLnm_0svxvnhT4SAXKADzu9RhhwAPUjg6J-fg6imM" })
         return token;
     } catch (err) {
         console.log(err);
